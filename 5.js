bd01c59155fe79e3d265284585b713a66e5ecb9f
John Weis <weis.john@gmail.com>
2014-05-14 14:53:47 -0400

    adding animation example

---
+++ 5.js
@@ -0,0 +1,16 @@
+var page = require('webpage').create();
+
+function format(num) { return num > 9 ? num : "0" + num; }
+
+var count = 0, duration = 750, frames = 100;
+var delay = duration / frames;
+
+function render() {
+    if (count > frames) return phantom.exit();
+    page.render('animation_' + format(count) + '.png');
+    count++;
+    setTimeout(render, delay);
+}
+
+page.viewportSize = page.clipRect = { width: 124, height: 124 };
+page.open('http://i.johnweis.com/_spin.html', render);
