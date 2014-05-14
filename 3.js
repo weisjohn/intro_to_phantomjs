6ed663c373f3cc66b64e6f5f9ec6e30588eed4bf
John Weis <weis.john@gmail.com>
2014-05-14 14:52:58 -0400

    a more helpful comment

--- 3.js
+++ 3.js
@@ -1,11 +1,10 @@
 var page = require('webpage').create();
 
 page.open('http://github.com/explore', function() {
-    
-    // super idomatic
+
     page.clipRect = page.evaluate(function() {
-        var selector = '.featured-grid'
-        var elem = document.querySelector(selector);
+        // runs in the "browser"
+        var elem = document.querySelector('.featured-grid');
         return elem.getBoundingClientRect();
     });
 
