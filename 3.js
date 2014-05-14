9218c22e00d3db81d3f2215032d12200a0d5db3d
John Weis <weis.john@gmail.com>
2014-05-14 14:12:55 -0400

    shorter element capture

--- 3.js
+++ 3.js
@@ -1,12 +1,5 @@
 var page = require('webpage').create();
 
-page.viewportSize = page.clipRect = { 
-    width: 1024, height: 768
-};
-
-var name = 'github.explore.';
-name += ~~(Math.random() * 1e3) + '.png';
-
 page.open('http://github.com/explore', function() {
     
     // super idomatic
@@ -16,6 +9,6 @@ page.open('http://github.com/explore', function() {
         return elem.getBoundingClientRect();
     });
 
-    page.render(name);
+    page.render('github.explore.' + Math.random() + ".png");
     phantom.exit();
 });
