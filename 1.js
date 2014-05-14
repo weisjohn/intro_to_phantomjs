856f3be02751c1df831072af30b6602409ab25df
John Weis <weis.john@gmail.com>
2014-05-14 13:24:35 -0400

    easy viewport awesomeness

--- 1.js
+++ 1.js
@@ -1,6 +1,9 @@
 var page = require('webpage').create();
-page.viewportSize = page.clipRect = { width: 1024, height: 768 };
-page.clipRect.left = page.clipRect.top = 0;
+
+page.viewportSize = page.clipRect = { 
+    width: 1024, height: 768
+};
+
 page.open('http://github.com/', function() {
     page.render('github.clip.png');
     phantom.exit();
