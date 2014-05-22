415aa5d95cfa3e836a8f5de5f9386fd629147232
John Weis <weis.john@gmail.com>
2014-05-21 20:38:35 -0400

    i solved it, finally

--- 10.js
+++ 10.js
@@ -3,32 +3,33 @@ var stderr = require('system').stderr;
 var stdin = require('system').stdin;
 
 function write(obj) {
-    stderr.write(JSON.stringify(obj));
+    stderr.write(JSON.stringify(obj) + "\n");
     if (obj.action !== "ack") read();
 }
 
 function read() {
     var data = stdin.readLine();
-    try { _read(JSON.parse(data)); }
+    try { data.split("\t").forEach(_read) } 
     catch (e) { throw new Error(data); }
 }
 
-function _read(data) { data.forEach(_readLine); }
+function _read(data) { return _readLine(JSON.parse(data)); }
 function _readLine(obj) {
+    // write({ "action": "ack", "obj": obj })
     if (obj.action !== "ack") write({ action: "ack", obj: obj });
     // TODO: invoke routes here
 }
 
 setInterval(function() {
-    write({ action : "heartbeat" });
-}, 1e3);
+    write({ "action" : "heartbeat" });
+}, 1e2);
 
 var page = require('webpage').create();
 page.open("http://github.com", function(status) {
     if (status == "success") {
-        write({ action: "opened" });
+        write({ "action": "opened" });
         page.render('github.jpg');
-        write({ action: "rendered" });
+        write({ "action": "rendered" });
     }
     phantom.exit();
 });
--- 9.js
+++ 9.js
@@ -3,31 +3,34 @@ var spawn = require('child_process').spawn;
 var phantomjs = spawn('phantomjs', ['10.js']);
 
 phantomjs.stderr.on('data', function(data) {
-    data = data.toString();
-    try { read(JSON.parse(data)); } 
-    catch (e) { throw new Error(data); }
+    data.toString().split('\n').forEach(_read);
 });
 
 phantomjs.on('exit', function() { console.log('done'); });
 
 var writeBuffer = [];
-function write(obj) {  writeBuffer.push(JSON.stringify(obj)); }
+function write(obj) { writeBuffer.push(JSON.stringify(obj)); }
 function _write() {
-    phantomjs.stdin.write(JSON.stringify(writeBuffer) + "\n");
+    phantomjs.stdin.write(writeBuffer.join("\t") + "\n");
     writeBuffer = [];
 }
 
+function _read(data) {
+    if (!data) return;
+    try { data = JSON.parse(data) } 
+    catch (e) { console.log(e); console.log(data); throw new Error(e); }
+    read(data);
+}
 function read(obj) {
     if (obj.action == "ack")
-        return console.log("phantom:", JSON.parse(obj.obj));
+        return console.log("phantom:", obj.obj);
     
-    write({ action: "ack" });
+    write({ "action": "ack" });
     // TODO: invoke routes here
     console.log("node:", obj);
     _write();
 }
 
-// can't yet do this
-// setInterval(function() {
-//     write({ action : "heartbeat" });
-// }, 1e3);
+setInterval(function() {
+    write({ action : "heartbeat" });
+}, 1e2);
