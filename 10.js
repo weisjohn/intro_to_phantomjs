1fe93b6e3652f42c537d68bb05e2f807b8fb6ce8
John Weis <weis.john@gmail.com>
2014-06-03 10:28:01 -0400

    rewriting examples to use sleepyhollow

--- 10.js
+++ 10.js
@@ -1,35 +1,15 @@
+var sleepyhollow = require('./node_modules/sleepyhollow-phantom/index.js');
+var mrhyde = sleepyhollow();
 
-var stderr = require('system').stderr;
-var stdin = require('system').stdin;
+mrhyde.on('render', function(url) {
+    var page = require('webpage').create();
+    page.open(url, function(status) {
+        page.render(url + ".png");
+        mrhyde.emit("rendered");
+        page.close();
+    });
+});
 
-function write(obj) {
-    stderr.write(JSON.stringify(obj) + "\n");
-    if (obj.action !== "ack") read();
-}
-
-function read() {
-    var data = stdin.readLine();
-    try { data.split("\t").forEach(_read) } 
-    catch (e) { throw new Error(data); }
-}
-
-function _read(data) { return _readLine(JSON.parse(data)); }
-function _readLine(obj) {
-    // write({ "action": "ack", "obj": obj })
-    if (obj.action !== "ack") write({ action: "ack", obj: obj });
-    // TODO: invoke routes here
-}
-
-setInterval(function() {
-    write({ "action" : "heartbeat" });
-}, 1e2);
-
-var page = require('webpage').create();
-page.open("http://github.com", function(status) {
-    if (status == "success") {
-        write({ "action": "opened" });
-        page.render('github.jpg');
-        write({ "action": "rendered" });
-    }
+mrhyde.on('end', function() {
     phantom.exit();
 });
--- 9.js
+++ 9.js
@@ -1,36 +1,9 @@
+var sleepyhollow = require('sleepyhollow-node');
+var drjekyll = sleepyhollow('10.js');
 
-var spawn = require('child_process').spawn;
-var phantomjs = spawn('phantomjs', ['10.js']);
-
-phantomjs.stderr.on('data', function(data) {
-    data.toString().split('\n').forEach(_read);
+drjekyll.emit('render', "http://github.com/");
+drjekyll.on('rendered', function() {
+    console.log('a page was rendered');
 });
 
-phantomjs.on('exit', function() { console.log('done'); });
-
-var writeBuffer = [];
-function write(obj) { writeBuffer.push(JSON.stringify(obj)); }
-function _write() {
-    phantomjs.stdin.write(writeBuffer.join("\t") + "\n");
-    writeBuffer = [];
-}
-
-function _read(data) {
-    if (!data) return;
-    try { data = JSON.parse(data) } 
-    catch (e) { console.log(e); console.log(data); throw new Error(e); }
-    read(data);
-}
-function read(obj) {
-    if (obj.action == "ack")
-        return console.log("phantom:", obj.obj);
-    
-    write({ "action": "ack" });
-    // TODO: invoke routes here
-    console.log("node:", obj);
-    _write();
-}
-
-setInterval(function() {
-    write({ action : "heartbeat" });
-}, 1e2);
+drjekyll.emit('end');
